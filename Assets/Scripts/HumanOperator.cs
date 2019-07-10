08e04e7e01f63f31c8eca8b76e02ca233648fca2
pm-ranj <payam.ranjbar@ut.ac.ir>
2019-07-10 14:35:39 +0430

    init without dialogue system

---
+++ Assets/Scripts/HumanOperator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanOperator : MonoBehaviour
+{
+
+    public KeyCode CorrectAnswerKey = KeyCode.T;
+    public KeyCode WrongAnswerKey = KeyCode.F;
+    public GameObject IconsCanvas;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(CorrectAnswerKey))
+        {
+            DialogueManager.instance.DisplayNextSentence();
+            DialogueManager.instance.PerformAfterEvent();
+        }
+
+        if (Input.GetKeyDown(WrongAnswerKey))
+        {
+            DialogueManager.instance.DisplayNextSentence(true);
+            DialogueManager.instance.PerformAfterEvent(true);
+        }
+        
+        
+    }
+}
---
+++ Assets/Scripts/SoundEffectsManger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+[Serializable]
+public class SoundEffectsManger : MonoBehaviour
+{
+    public SoundEffect[] Effcets;
+
+    public void play(string n)
+    {
+        var effect = Array.Find(Effcets, Effects => Effects.name == n);
+        effect.play();
+
+    }
+
+    public void stop(string n)
+    {
+        var effect = Array.Find(Effcets, Effects => Effects.name == n);
+        effect.stop();
+    }
+
+    internal void play(int index)
+    {
+        throw new NotImplementedException();
+    }
+}
